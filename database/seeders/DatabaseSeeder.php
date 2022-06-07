eaadf6c97e285348358e6492d89d9d3e6a5a46a4
Mustafa Palta <Mustafapalta00@hotmail.com>
2022-06-07 19:54:15 +0300

    Question Migrate Question Factory-Seed

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,8 @@
 
 namespace Database\Seeders;
 
+use App\Models\Quiz;
+use App\Models\Question;
 use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -19,6 +21,7 @@ class DatabaseSeeder extends Seeder
         $this->call([
             UserSeeder::class,
             QuizSeeder::class,
+            QuestionSeeder::class,
         ]);
         // \App\Models\User::factory()->create([
         //     'name' => 'Test User',
